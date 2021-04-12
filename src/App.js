63a4c241542a3adf6fba60e93cfcfdfc991deb86
Norbert Krupa <toftnqr@gmail.com>
2021-04-12 15:29:47 +0200

    add things to gh-pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
   render() { 
     return ( 
-     <Router>
+     <Router basename = {process.env.PUBLIC_URL}>
        <div className="container">
          <header>
            <Header />
